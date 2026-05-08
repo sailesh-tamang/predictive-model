162fc79f9a392c1ad92ae82b5b2b1cee6c6ca83b
sailesh-tamang <saileshtamang909@gmail.com>
2026-05-08 09:44:07 +0545

    Fix: Add basename to React Router for GitHub Pages subdirectory routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import './styles/main.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/predictive-model">
       <App />
     </BrowserRouter>
   </React.StrictMode>
